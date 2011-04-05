38f164f2d27d54b6ab8741ea1cb8d88a38598008
Maksim Horbul <max_horbul@playstation.sony.com>
2011-04-04 18:48:14 -0700

    use recent rvm installation method

--- definitions/centos-5.5-i386/postinstall.sh
+++ definitions/centos-5.5-i386/postinstall.sh
@@ -4,23 +4,20 @@
 rpm -Uvh --force --nosignature --nodigest http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
 rpm -Uvh --force --nosignature --nodigest http://download.elff.bravenet.com/5/i386/elff-release-5-3.noarch.rpm
 
-yum install -y curl ftp rsync sudo time wget which git-core
+yum install -y bash curl ftp rsync sudo time wget which git
 yum install -y gcc bzip2 make kernel-devel-`uname -r`
-yum install -y gcc-c++ zlib-devel openssl-devel readline-devel
+yum install -y gcc-c++ patch readline-devel zlib-devel libyaml-devel libffi-devel openssl-devel iconv-devel
+
 yum erase -y wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 
 yum -y clean all
 
 cd /tmp
-wget --no-check-certificate https://github.com/wayneeseguin/rvm/raw/1.3.0/contrib/install-system-wide
-sh install-system-wide --version '1.3.0'
-rm -f install-system-wide
-
-cat > /etc/profile.d/rvm.sh <<\EOF
-[[ -s '/usr/local/lib/rvm' ]] && source '/usr/local/lib/rvm'
-EOF
-
-source '/usr/local/lib/rvm'
+curl -L http://rvm.beginrescueend.com/install/rvm -o rvm-install
+bash rvm-install
+rm -rf rvm-install
+source '/usr/local/rvm/scripts/rvm'
+rvm get latest
 
 rvm use 1.9.2 --install --default
 rvm default exec gem install chef --no-ri --no-rdoc
@@ -30,10 +27,9 @@ rvm default exec gem install puppet --no-ri --no-rdoc
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
+curl -L http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub -o authorized_keys
 chmod 400 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
-
 
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
